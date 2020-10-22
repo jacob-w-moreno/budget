0055d6aaf0bea81b3068e555f6109e1bd38a575f
Jake <jacob.w.moreno@gmail.com>
2020-10-22 15:54:30 -0600

    I need to work on styling...

--- src/Components/Budget/DollarEdit.js
+++ src/Components/Budget/DollarEdit.js
@@ -5,15 +5,27 @@ const BudgetListEdit = (props) => {
 
 // === === === JSX START === === ===
 
-  const balanceInput = <input type='number' value={props.allocated} onChange={(e)=>{props.editAllocation(props.index, +e.target.value)}}/>
-  const nameInput = <input value={props.name} onChange={(e)=>{props.editName(props.id, e.target.value)}}/>
+  const balanceInput = <input className='edit__input'
+    type='number'
+    value={props.allocated}
+    onChange={(e)=>{props.editAllocation(props.index, +e.target.value)}}/>
+
+  const nameInput = <input className='list-display edit__input'
+    value={props.name}
+    onChange={(e)=>{props.editName(props.id, e.target.value)}}/>
 
 // === === === JSX END === === ===
 
   return (<div className='dp-list-item'>
-    $ {balanceInput}
-    {nameInput}
-    {props.balance}
+    {/* <div className='list-display'> */}
+      $ {balanceInput}
+    {/* </div> */}
+    {/* <div className='middle list-display'> */}
+      {nameInput}
+    {/* </div> */}
+    {/* <div className='right list-display'> */}
+      {props.balance}
+    {/* </div> */}
   </div>
   )
 }
